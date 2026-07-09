55fdb6c7c2ad4eba3bab039abe02c9cfbd23d1a1
Hugo Lemos <hugolemos02@yahoo.com.br>
2026-07-09 14:44:00 -0300

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,14 @@
-<h1>
-    Welcome to the Home Page
-</h1>
-
-<p>Olá, {{$name}}!</p>
-<p>Seus hábitos são:</p>
-
-<ul>
-    @foreach($habits as $item)
-        <li>{{$item}}</li>
-    @endforeach
-</ul>
-
-@auth
-    <p>Você está logado!</p>
-@endauth
-
-@guest
-    <p>Você não está logado!</p>
-@endguest
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-3x1 font-bold underline">
+        Hello, world!
+    </h1>
+</body>
+</html>
