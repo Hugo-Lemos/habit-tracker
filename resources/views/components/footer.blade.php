03b35e9ad48faafff62ebdb79394514756208f66
Hugo Lemos <hugolemos02@yahoo.com.br>
2026-07-09 17:56:10 -0300

    feat: init components header/footer/layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4">
+    <p class="text-center">
+        Criado por Hugo Lemos. <a href="#" class="underline" >Github</a> 
+    </p>
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name') }}</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    <h1 class="text-3x1 font-bold underline">
-        Hello, world!
-    </h1>
-</body>
-</html>
+<x-layout>
+    <main class="py-10">
+        <h1 class="text-center">
+            Veja seus hábitos ganharem vida
+        </h1>
+    </main>
+</x-layout>
