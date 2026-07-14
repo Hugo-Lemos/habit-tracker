55ab2ca916d770ce9e4e9074ea91bfa9d2a18b77
Hugo Lemos <hugolemos02@yahoo.com.br>
2026-07-14 11:02:34 -0300

    feat(Auth): Add login page and controller

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-white border-t-2 p-4">
     <p class="text-center">
-        Criado por Hugo Lemos. <a href="#" class="underline" >Github</a> 
+        Criado por Hugo Lemos. <a href="https://github.com/Hugo-Lemos/" class="underline" >Github</a> 
     </p>
 </footer>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
+//Site
 Route::get('/', [\App\Http\Controllers\SiteController::class, 'index']);
+
+//Login
+Route::get('/login', [LoginController::class, 'index']);
